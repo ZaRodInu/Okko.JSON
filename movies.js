c635cc4e2831e7e95e548485000e3fca789aea1b
Muhammad <patsangeniy@gmail.com>
2023-06-25 16:45:09 +0500

    movies ready

---
+++ movies.js
@@ -0,0 +1,267 @@
+//  Movies
+// 1
+const movie = [
+  {
+    img: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABVCVplfWKDjc1vPwoGxnUKBNglFFHWI8ffsRyv9_Mgcv4I6PtKdqk2fPiPxb5e8FToHlkx8Ui-oC72H8a69nCt-LobTv7PyDw4Hs.jpg?r=844",
+    logo: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABYyHF6sDpwCna5qBbJX_pZyMTVBo4vkZB9LClD_mRMLoJnUd-QSSzfug615bSJ0x_iEijobZ0DM45PR7rwpq9SzO_e9Ch4Z6uJwwkZiW-rtclBhmnKGMurcjaPW96eJ6hcGgvb-kh_0_5vyxyVgTUO8xWM_k-bEpTeKPCzkJl2w_YI-XriYOaA.png?r=83b",
+    name: "Шутки в сторону (2022)      ",
+    rating: 6.4,
+    id:1 , 
+    released: "2022",
+    categories: "Экшен",
+    time: "2 ч. 1 мин.",
+    lg: " Rus, Eng",
+    ageLimit: "18+",
+    description:
+      "Спустя десять лет такие непохожие напарники снова берутся за дело. Расследуя убийство в неспокойном французском городке, они разоблачают масштабный заговор.",
+    director: "Чад Стахелски",
+    actors: "Омар Си,Лоран Лафитт,Изиа Ижлен",
+    detailedDescription:
+      "Полицейские Усман и Франсуа снова в деле. Им вновь придётся поработать вместе и расследовать убийство парня, которого разрезало пополам. ",
+    comments: [
+      {
+        name: " Alex Croft",
+        description:
+          "В далеком 2012 году свет увидел «Шутки в сторону» режиссера Давида Шарона. Не смотря на то, что фильм получился достаточно не плохим, кассовым хитом он не стал и получил достаточно прохладный приём как среди критиков, так и среди зрителей. Отсюда и не удивительно, что разговоры о каком-либо продолжении быстро сошли на нет. Как бы там ни было, спустя десять лет режиссер Луи Летерье решил изменить ситуацию. Стоило ли оно того и способен ли фильм запустить полноценную франшизу лучше, чем его предшественник? Давайте разбираться.",
+      },
+    ],
+  },
+  //2
+  {
+    img: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABUZiNlWIg-Saotmb31RKJCl1cWdLhikMT5CpY_obyjljdtdWMQ5_jC4NxTJoNkUsuTmnXSoamkPVV20-H7j8ODTsgQp4bJCbiub_.jpg?r=835",
+    logo: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABRsdzlxGL3jLown_AewipZTtcJnGk-IuqGPFKNthWtoRSu2f8i4UwnBDMdyNf882HDucIOlKn4HRPeEqM-QTEWRV4viyh2qDPGAo4z0MHHe-.png?r=1db",
+    name: "Каратель ",
+    rating: 6.7,
+    id:2 , 
+    released: "2022",
+    categories: "боевик, триллер, драма, криминал",
+    time: "2 ч. 3 мин.",
+    lg: " Rus, Eng",
+    ageLimit: "18+",
+    description:
+      "Фрэнк Касл — агент ФБР под прикрытием, который после долгих лет службы оперативником должен наконец-то получить кабинетную должность и начать спокойную жизнь со своей женой и детьми. Однако ему предстоит пережить страшное потрясение — на его глазах от рук преступника погибает вся его семья. Несмотря на душевную боль, Фрэнк начинает войну против тех, кто лишил его самого дорогого в жизни.",
+    director: "Джонатан Хенсли",
+    actors: "Томас Джейн,Джон Траволта,Уилл Пэттон",
+    detailedDescription:
+      "Главные роли в этой экранизации всеми любимой истории Marvel Comics о Карателе сыграли Джон Траволта и Томас Джейн.",
+    comments: [
+      {
+        name: " ComingBack",
+        description:
+          "Итак, начнём с сюжета. А он в 'Карателе' такой - человек, работавший на спецслужбы долгое время, наконец, выходит на заслуженный отдых и получает время, которое он хотел уделить своей семье... Для него должна наступить идиллия - он так долго хотел этого. Но на последнем заданий его напарники подстреливают человека, который... был сыном одного известного босса Мафии.",
+      },
+    ],
+  },
+  //3
+  {
+    img: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABdoDaLYTPfqkJJyXgS9GKpqkN46TYyn3V9W1ILX-oEb0HIqMPmZNYa0CEFgU7oS-_jLpmMfann7WopROcGHMVJGXrAtnPAI4DLsE.jpg?r=eeb",
+    logo: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABaT2dh5Htu07tP5ofYz_aHbFPY3PHRJ8r45BqzB0uDB8QXMo9cWbeZ9mRCbLXcdafuu5M6KYcdrv5hBWa3LPUMCj_NdI308YkgViHKz5gfrROFAaFrnqo3b1JRXFgNjwIryIuAdG7neeCFwAIiK15arx7k6uioT4Qs2I1TtTShhU6e4zpmnPLA.png?r=d08",
+    name: "Черный краб ",
+    rating: 5.8,
+    id:3 , 
+    released: "2022",
+    categories:
+      "фантастика, боевик, триллер, драма, приключения, военный слова",
+    time: "1 ч. 54 мин.",
+    lg: " Rus, Eng",
+    ageLimit: "16+",
+    description:
+      "Чтобы положить конец апокалиптической войне и спасти дочь, женщина-солдат соглашается на опасное задание. Ей предстоит пересечь замерзшее море и доставить секретный груз.",
+    director: "Адам Берг",
+    actors: "Нуми Рапас,Якоб Офтебро,Эрик Энге",
+    detailedDescription:
+      "В постапокалиптическом мире шесть солдат, выполняющих секретную миссию, должны перевезти особый груз через замерзший архипелаг",
+    comments: [
+      {
+        name: " Вкушающий",
+        description:
+          "На коньках с двадцати килограммовой амуницией за плечами? По морскому льду? Рывок в несколько сотен километров? Скрытное передвижение преимущественно ночью? Быть обнаруженным - значит погибнуть? Кому такое под силу?",
+      },
+    ],
+  },
+  // 4
+  {
+    img: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABWZFg5Dwb_3DsGXmb1Jl0yN3gnBLK3hpRvGrK93w-zQSEdFH1c3P89fhWUiSRitj62W-CX_o7Rmnl-Ik_W5w6jBEH18rG28FTHCL.jpg?r=b6a",
+    logo: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABUHp5LMXxJofj-Q-ctU2q_a5BE2t1RzA_8SOy7v42mkknwDfMwHdw_V3NHyn1aiz5OcN3CvhygRNAm0MtMZkg4W_ByORyk7Gfl07B5aaG9Ho7K-0xRYbw_-tIURDYT0xIuEPM5bOvYzgSUGmBddlTF62EfevqX8YmbkT3lF0IP0LNuKl7ZB-Pg.png?r=977",
+    name: "Маугли — легенда джунглей",
+    rating: 6.3,
+    id:4, 
+    released: "2018",
+    categories: "фэнтези, драма, приключения",
+    time: "1 ч. 44 мин.",
+    lg: " Rus, Eng",
+    ageLimit: "13+",
+    description:
+      "На пути к предназначению сирота, воспитанный животными в джунглях, вступает в схватку с опасным противником и противостоит своей человеческой природе.",
+    director: " Энди Серкис",
+    actors: "Кристиан Бэйл,Кейт Бланшетт,Бенедикт Камбербэтч",
+    detailedDescription:
+      "История о взрослении ребёнка по имени Маугли в волчьей стае в джунглях Индии. Он учится суровым законам джунглей под опекой медведя Балу и пантеры Багиры и принимает обитателей диких джунглей за своих. Всех, кроме одного: страшного тигра Шерхана. Но вскоре мальчику предстоит узнать, что в джунглях его могут поджидать куда большие опасности, чем тигр, и одна из них - его собственное человеческое происхождение, с которым ему придётся столкнуться лицом к лицу, словно с зубастым хищником.",
+    comments: [
+      {
+        name: " vvf2008",
+        description:
+          "Восприятие данного творения пропустил через призму 'Ожидание/Действительность'. Базировался на книге, советском мультике, диснеевском и прежней экранизации. Увиденное не просто удивило - взорвало мозг. Сразу скажу - я сторонник мультипликационной версии 1973 года: сильная и умная Багира (а голос, голос Касаткиной!), заботливый и трогательный Балу, мудрый и загадочный Каа, независимые и гордые (не все) волки, взрослеющий и набирающий силу Маугли, который в итоге все же осознает, что он человек, и гордится этим. Неплоха и версия 2016 года - и сюжет и графика - все на высоте.",
+      },
+    ],
+  },
+  // 5
+  {
+    img: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABQMzncl0HCHYWkBX02smp5Me_E4DPVpJKh-fFGgn_z_qygGtketexwoAugSAPfAyS6g6CbNnw2xvUW2V2EJCHRqSX9JYovxrf6g91VtVA2nBJsv9jdyeNvtWzVKXukIRPKDkykAaRR5_TzPVf3szsPhTPeBhaFbSILIbecWaZoGlK8MTG7xbHw.png?r=73d",
+    logo: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABUHp5LMXxJofj-Q-ctU2q_a5BE2t1RzA_8SOy7v42mkknwDfMwHdw_V3NHyn1aiz5OcN3CvhygRNAm0MtMZkg4W_ByORyk7Gfl07B5aaG9Ho7K-0xRYbw_-tIURDYT0xIuEPM5bOvYzgSUGmBddlTF62EfevqX8YmbkT3lF0IP0LNuKl7ZB-Pg.png?r=977",
+    name: "Имя мне Вендетта",
+    rating: 6.7,
+    id:5 , 
+    released: "2022",
+    categories: "Экшен",
+    time: "1 ч. 32 мин",
+    lg: " Rus, Eng",
+    ageLimit: "18+",
+    description:
+      "Старые враги бывшего киллера-мафиози убивают его жену и шурина. Вместе с дочерью он скрывается в Милане и продумывает план мести.",
+    director: " Алессандро Гассман",
+    actors: "Алессандро Гассман,Джиневра Франческони,Ремо Джироне",
+    detailedDescription: "",
+    comments: [
+      {
+        name: " guest",
+        description: "soou cool",
+      },
+    ],
+  },
+  // 6
+  {
+    img: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABbp4UPoHiX6BdGCg7Oq61k2Wk5rlTi4xI8SRZZNXeC3YIxfjXt9MlixuL6RSjzcV1VUYKxsd8rc7teBEeiPK7n_NhCjtVQ8oonXV.jpg?r=9f9",
+    logo: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABV5txsANl_nDflb7rThnD5XHQv7r1IH0tsDi5dSD5P74fOUQzbKmhIB3emT7kqziy3-D-j960GIIhK6Jr2Nx6Irp-RN8nzlU-1XTZ0jpdDOncbXwybz7dS7dDjFiSlxHyYqM6P6aewBFMyGPnjN--ZTXvkBx76F3lCRwQM5M-bZU0PjJo3_WNA.png?r=bb8",
+    name: "Дневная смена",
+    rating: 6.3,
+    id:6 , 
+    released: "2022",
+    categories: "ужасы, боевик, комедия",
+    time: "1 ч. 53 мин.",
+    lg: " Rus, Eng",
+    ageLimit: "18+",
+    description:
+      "У охотника на вампиров из Лос-Анджелеса всего неделя, чтобы найти деньги на оплату обучения и брекетов ребенка. В попытках заработать на жизнь как бы ее не лишиться!",
+    director: " Дж.Дж. Перри",
+    actors: "Джейми Фокс,Дэйв Франко,Наташа Лю Бордиццо",
+    detailedDescription:
+      "Долина Сан-Фернандо. С виду Бад Яблонски — обычный чистильщик бассейнов, но на самом деле он выслеживает и убивает вампиров, а также выдирает у них клыки, чтобы потом повыгоднее продать. Этим и живёт. Ещё Бад души не чает в 10-летней дочке, поэтому когда он узнаёт, что из-за нехватки денег бывшая жена собирается продать дом и увезти девочку к маме во Флориду, то решает быстро раздобыть необходимую сумму. А для этого Баду нужно срочно вернуться в профсоюз истребителей вампиров, из которого его выперли за многочисленные нарушения кодекса.",
+    comments: [
+      {
+        name: " micro1941",
+        description:
+          "Режиссерский дебют Перри вряд ли может привнести что то новое в тематику охоты на вампиром. У Бада Яблонски в простонародье зубная фея благородная цель - не дать матери его ребенка забрать дочь во Флориду. Использование всеми правдами и неправдами тематика семьи не на столько раздражающий фактор, как присутствие шуточек про историю афроамериканцев, и те меркнут перед кривлянием Франко, отыгрывающего тинейджера и напарника Бада с целью дальнейшего повышения в союзе по истреблению вампиров. Не обходится фильм без вульгарных и бетонных шуточек про анусы и мерзких кривляний Франко, показывающего крики белок. Блеклый юмор Сета отпечатывается на итак безэмоциональном лице Фокса. ",
+      },
+    ],
+  },
+  // 7
+  {
+    img: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABbuR7siKWje_DEp4wRQ3kx3rGkokwSI9AINodbxwByrQyl6o92GZH0py99I6o0paiOtjfV1bq4sZKEnUenVD-pw_082-cYQVA5mw.jpg?r=716",
+    logo: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABettN3s9Ee74rKISCQGGV7erkmEhTzQ9hbEUlXH7ZrPVC-8fj24pPMLaiZykY_EXYKUoCju5KXfqORdKzLv3HmX2gVsjyZAuqEPLoa6mxErM78Ahu-f1Po4G4lDp8Ax97pFMs_b7exb-W6UySRNvsZmj3pFh9Vw5z-GgosLNk55rGd_EwXCfEg.png?r=d81",
+    name: "Расплата за грехи",
+    rating: 4.9,
+    id: 7 , 
+    released: "2022",
+    categories: "триллер, драма, мелодрама, криминал, детектив",
+    time: "1 ч. 36 мин.",
+    lg: " Rus, Eng",
+    ageLimit: "18+",
+    description:
+      "Автор детективов Грейс Миллер обладает феноменальной способностью определять мотив преступления. Ей очень пригодится этот талант, чтобы раскрыть убийство сестры.",
+    director: " Моника Митчелл",
+    actors: "Алисса Милано,Сэмюэл Пейдж,Эмили Уллерап",
+    detailedDescription: "",
+    comments: [
+      {
+        name: "fruit.ko",
+        description:
+          "На этот фильм у меня стояло оповещение на Нетфликсе, ведь красотку Алису Милано в Нетфликсовском детективе мы ещё не видели. Более того, актриса 5 лет не снималась в полном метре, так что предвкушение фанатов было ожидаемо. Но лучше бы мы этого не видели...",
+      },
+    ],
+  },
+  // 8
+  {
+    img: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABYk9X3HtG7j0vMCo2rm1MHpBdob_UyLqzeUOk349bHRwWAbNnwoZtSOKE7exXzyf_0IwSZVOaqip9yYvZFnJV5oCBist0v0JN-ZU.jpg?r=447",
+    logo: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABYMFyQVDTq0d3n8OxhCKZg3KFXMQOogogMvZcgNT3DMlWehd4YY8C5rc3EsP0hwrmKgSwyJ1lHAqhKtH1NN_2M_Kkhw-DkmjaeBINYvt_H7azaoMJPFppcnHdBQgky3S-fBHPjyaQYiA5nj1vpo4kpdhAztGxYgvV4LNHqz2RoxNoCPScA4_UQ.png?r=a66",
+    name: "Поездка на выходные",
+    rating: 6.2,
+    id: 8 , 
+    released: "2022",
+    categories: "триллер, драма, криминал, детектив",
+    time: "1 ч. 31 мин.",
+    lg: " Rus, Eng",
+    ageLimit: "16+",
+    description:
+      "Лучшая подруга Бет исчезает в Хорватии, где женщины устраивают девичник, и героиня спешит выяснить правду. С каждой новой зацепкой ей открывается все более жуткий обман.",
+    director: " Ким Фаррант",
+    actors: "Лейтон Мистер,Кристина Вульф,Зиад Бакри",
+    detailedDescription: "",
+    comments: [
+      {
+        name: "ЧестноеКино",
+        description:
+          "Присказка про двух подружек, решивших отметить девичник в Хорватии. Залив алкоголем свои похотливые зенки, девчули знакомятся с парочкой мужиков, а на утром главная героиня Бэт просыпается в отеле в беспамятстве. Она лихорадочно пытается понять куда подевалась ее подруга и что случилось прошлой ночью, но чем больше она узнает, тем сильнее история становится запутанной...",
+      },
+    ],
+  },
+  // 9
+  {
+    img: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABTdW719PPqaGQ63azDIQ2MTz1sOBTZk-J9stK0-8C0JYpkwy0iZB0COyaf4s9uzmoLH7MZH3iS2fS6rnnvdSXSfq4w_jiE8Wwzf2.jpg?r=e45",
+    logo: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABbcDDrlXG_U6PQ8GOtzh0yZvK0qyRJz2sAffdL8ZzFeC5fP8AiElAPmSkdU5w6i0XgdLOT3OKUVH8GxyI0MTdzPz_gZ2BneFPUHSbnSdpJy2C88TRkGb0EW6G70sTbZU_PW3T5lAOnLwkjiI4t7OCSaAAfOuocPDkCcwWeXn5d_q6x95068hNw.png?r=788",
+    name: "Женщина в окне  ",
+    rating: 5.9,
+    id: 9, 
+    released: "2021",
+    categories: "триллер, криминал, детектив",
+    time: "1 ч. 41 мин.",
+    lg: " Rus, Eng",
+    ageLimit: "18+",
+    description:
+      "Психолог, вынужденная сидеть дома из-за агорафобии, начинает следить за своими новыми соседями и пытается раскрыть жестокое преступление, которое она видела из окна.",
+    director: " Джо Райт",
+    actors: "Эми Адамс,Гэри Олдман,Энтони Маки    ",
+    detailedDescription: "Женщина, страдающая агорафобией, начинает шпионить за своими новыми соседями и однажды становится свидетельницей убийства. Но ей никто не верит.",
+    comments: [
+      {
+        name: "ticketer",
+        description:
+          "Да, тут мы увидим весь стандартный и близкий сердцу любителя детективов набор атрибутов: женщина в неврозе, увиденное ею притупление и неверие полиции, а также немного чокнутые и пугающие соседи. Беда в том, что этот набор мы видели так часто, что для цепляющего эффекта он должен быть как-то оригинально подан, но тут мы видим нечто усреднённое, как стандартные обои на заставке компьютера. Эми, как обычно, естественна и её героине можно сочувствовать, но в просчитывающемся на два шага вперёд сценарии ей сложно чем-то заворожить зрителя. Даже складывается такое ощущение, что режиссёр хотел сделать ставку не столько на интригу, сколько на настроение - замкнутое пространство огромного",
+      },
+    ],
+  },
+  // 10
+  {
+    img: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABd0pc--YwrszEFskvTdcSyUhOvf0pw-9gvxtznVkGRmglP3njRKrKU81sje6PbeYVPFdSIRpVvd22ruzYEM6NlNZzXSnhKVSHFAQ.jpg?r=fbc",
+    logo: "https://occ-0-768-395.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABYh4rrGeMxqLYhGbbOU0LwzurkbEGVNA-p43ymtHtA-ocr3CJVulrjBtAOyHwE008sYdwfztNOgrD9Exuj_WnNJs0ZBK3vmSlHLfyGY4VSFAYeyglkIJod39DhtOcJWKsTpYGeL2hK4tVv2ErVKiEU4pCnXv6RPiKGoXoL02nyKyQNOHuMoW5A.png?r=ca9",
+    name: "Травма",
+    rating: 5.9,
+    id: 10, 
+    released: "2019",
+    categories: "триллер",
+    time: "1 ч. 41 мин.",
+    lg: " Rus, Eng",
+    ageLimit: "16+",
+    description:
+      "Мужчина бросается на поиски своих жены и дочери, пропавших прямо из реанимации. Вскоре он понимает, что больница скрывает самые зловещие тайны.",
+    director: "",
+    actors: "Сэм Уортингтон,Лили Рэйб,Стивен Тоболовски    ",
+    detailedDescription: "Сэм Уортингтон (Аватар) играет главную роль в триллере режиссера Брэда Андерсона (Машинист, Транссибирский экспресс).",
+    comments: [
+      {
+        name: "",
+        description:
+          "",
+      },
+    ],
+  },
+];
+
+
+
+
+
+
+
+
+
